fe6fe9374abb4532541f97b4618683988a52ecb0
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2024-03-02 13:25:20 +0500

    fixed test9

--- haskell/exam/permuteArray2.hs
+++ haskell/exam/permuteArray2.hs
@@ -3,7 +3,7 @@ import Test.HUnit
 
 test7 = TestCase (assertEqual "for ," [1,2,3,4,5,6,7,8,9,10,11,12,14, 13] (permuteArray [1,2,3,4,5,6,7,8,9,10,11,12,13,14]))
 test8 = TestCase (assertEqual "for ," [1,2,3,4,5,6,7,8,9,10,11,12,14, 1,2,3,13] (permuteArray [1,2,3,4,5,6,7,8,9,10,11,12,13,14,3,2,1]))
-test9 = TestCase (assertEqual "for ," [1,2,3,4,5,6,7,8,9,10,11,12,13,14,3,2,3,1,1,2] (permuteArray [1,2,3,4,5,6,7,8,9,10,11,12,13,14,3,2,1,3,2,1]))
+test9 = TestCase (assertEqual "for ," [1,2,3,4,5,6,7,8,9,10,11,12,13,14,3,2,2,1,1,3] (permuteArray [1,2,3,4,5,6,7,8,9,10,11,12,13,14,3,2,1,3,2,1]))
 
 
 main :: IO ()
